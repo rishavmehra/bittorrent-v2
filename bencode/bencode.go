2d7e5993288eb978f1acfd82c03b450e7750fcf5
Rishav Mehra <rishavmehra61@gmail.com>
2024-10-23 02:54:59 +0530

    reimplemented Bencode String

--- bencode/bencode.go
+++ bencode/bencode.go
@@ -6,11 +6,16 @@ import (
 	"unicode"
 )
 
-func DecodeBencode(bencodedString string) (interface{}, error) {
-	if unicode.IsDigit(rune(bencodedString[0])) {
+// 5:hello => hello
+// 6:rishav => rishav
+func DecodeBencodeString(bencodedString string) (string, int, error) {
+	index := 0
 
+	switch {
+	case unicode.IsDigit(rune(bencodedString[0])):
 		var colonIndex int
 
+		// this give us colon index
 		for i := 0; i < len(bencodedString); i++ {
 			if bencodedString[i] == ':' {
 				colonIndex = i
@@ -18,16 +23,24 @@ func DecodeBencode(bencodedString string) (interface{}, error) {
 			}
 		}
 
+		// string(length number of the string) before the colon
 		lengthStr := bencodedString[:colonIndex]
+
+		// convert the length number of string ASCII to integer
 		length, err := strconv.Atoi(lengthStr)
 		if err != nil {
-			return "", err
+			return "", index, err
 		}
 
-		return bencodedString[colonIndex+1 : colonIndex+1+length], nil
-	} else if rune(bencodedString[0]) == 'i' {
-		return strconv.Atoi(bencodedString[1 : len(bencodedString)-1])
-	} else {
-		return "", fmt.Errorf("only string are supported at the moment")
+		index = colonIndex + 1 + length
+		decodedString := bencodedString[colonIndex+1 : index]
+		return decodedString, index, nil
+	default:
+		return "", 0, fmt.Errorf("invalid BecodeString %q", bencodedString)
 	}
+}
+
+func decodeBencodeInteger(bencodedString string) (int, int, error) {
+	index := 0
+
 }
